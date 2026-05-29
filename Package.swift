709a5941d351eb0cc7210886d114b7a41ac709ad
Haoyuan Kevin Xia <kevin062397@outlook.com>
2026-05-28 17:41:06 -0700

    Set up the Swift package with iOS 17 minimum target

--- Package.swift
+++ Package.swift
@@ -5,16 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPickerKit",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .visionOS(.v1)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftPickerKit",
             targets: ["SwiftPickerKit"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftPickerKit"
         ),
