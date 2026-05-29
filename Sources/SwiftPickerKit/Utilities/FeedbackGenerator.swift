f0071925b6e0d82c719bd46ed7e816854089e468
Haoyuan Kevin Xia <kevin062397@outlook.com>
2026-05-28 19:41:34 -0700

    Implement the haptic feedback generator utility

---
+++ Sources/SwiftPickerKit/Utilities/FeedbackGenerator.swift
@@ -0,0 +1,46 @@
+//
+//  FeedbackGenerator.swift
+//  SwiftPickerKit
+//
+
+#if canImport(UIKit)
+    import UIKit
+
+    enum FeedbackGenerator {
+        // MARK: - Public Functions
+
+        static func impact(_ feedbackStyle: UIImpactFeedbackGenerator.FeedbackStyle, intensity: CGFloat? = nil, at location: CGPoint? = nil) {
+            let generator = UIImpactFeedbackGenerator(style: feedbackStyle)
+            if let intensity = intensity, let location = location {
+                if #available(iOS 17.5, *) {
+                    generator.impactOccurred(intensity: intensity, at: location)
+                } else {
+                    generator.impactOccurred(intensity: intensity)
+                }
+            } else if let intensity = intensity {
+                generator.impactOccurred(intensity: intensity)
+            } else if let location = location {
+                if #available(iOS 17.5, *) {
+                    generator.impactOccurred(at: location)
+                } else {
+                    generator.impactOccurred()
+                }
+            } else {
+                generator.impactOccurred()
+            }
+        }
+
+        static func notify(_ feedbackType: UINotificationFeedbackGenerator.FeedbackType, at location: CGPoint? = nil) {
+            let generator = UINotificationFeedbackGenerator()
+            if let location = location {
+                if #available(iOS 17.5, *) {
+                    generator.notificationOccurred(feedbackType, at: location)
+                } else {
+                    generator.notificationOccurred(feedbackType)
+                }
+            } else {
+                generator.notificationOccurred(feedbackType)
+            }
+        }
+    }
+#endif
